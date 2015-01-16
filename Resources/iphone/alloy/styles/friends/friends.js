e228432bf44ea7f72f57a25f2b19899aa2192e72
hari gadiraju <harigadiraju@harigadrajusmbp.usaw.avaya.com>
2015-01-16 12:13:18 -0600

    side menu added 1.1

---
+++ Resources/iphone/alloy/styles/friends/friends.js
@@ -0,0 +1,1 @@
+module.exports = [{"isApi":true,"priority":1000.0162,"key":"Window","style":{backgroundColor:"#fff",navBarHidden:true,}},{"isId":true,"priority":100000.0163,"key":"main","style":{top:10,separatorStyle:Alloy.Globals._params.TableViewSeparatorStyle.NONE,backgroundColor:"#fff",}}];
